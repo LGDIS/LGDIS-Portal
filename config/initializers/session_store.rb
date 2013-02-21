1fcc65402253264a53b37d1ffcd568789de6ab79
Masaru Sato <marphy.986@gmail.com>
2013-02-21 13:41:38 +0900

    fix using cookie key.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-LgdisPortal::Application.config.session_store :cookie_store, key: '_LGDIS-Portal_session'
+LgdisPortal::Application.config.session_store :cookie_store, key: '_LGDIS_Portal_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
