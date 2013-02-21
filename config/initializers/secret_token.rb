5b0e08a220d708f0c040cb8370eb97d7f4913a7d
Masaru Sato <marphy.986@gmail.com>
2013-02-21 11:22:02 +0900

    Refinery CMS initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LGDISPortal::Application.config.secret_token = 'd5d283b5486177b30bf20ee85f4ea81ad8484361ed67b3fdf1fbec5fcbefb8dd845f03f3d61dbe411c91c0e38b93a3d34c684fa35362c2ccf60d6048acb48502'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LGDISPortal::Application.config.session_store :cookie_store, key: '_LGDIS-Portal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LGDISPortal::Application.config.session_store :active_record_store
