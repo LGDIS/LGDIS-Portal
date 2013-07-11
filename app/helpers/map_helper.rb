26d61d107cfc6669df29682ecd1898a84d49065f
Kentaro Shigeno <kenta@nttpc.co.jp>
2013-07-11 13:34:25 +0900

    added file app/helpers/map_helper.rb

---
+++ app/helpers/map_helper.rb
@@ -0,0 +1,29 @@
+module MapHelper
+
+  def js_for_georss_map(_feeds)
+    html = ''
+    if @types.include?("georss")
+      html += 'putRssMarkers();' + "\n"
+      if _feeds.status == 0
+        _feeds.items.each do |item|
+          if item.point.present?
+            s = ''
+            if item.title.present?
+              s += '<div style="font-family: Arial, sans-serif; font-size: small">'
+              s += '<div style="font-weight: bold; font-size: medium; margin-bottom: 0em">' + item_escape(item.title) + '</div>'
+              s += '<div><p>' + item_escape(item.content) + '</p></div>' if item.content.present?
+              s += '</div>'
+            end
+            html += 'setRssMarker(' + item.point.tr(" ", ",") + ', \'' + s + '\');' + "\n"
+          end
+        end
+      end
+    end
+    html
+  end
+
+  def item_escape(text)
+    # delete CR and LF, escape Single quato
+    text.gsub(/(\r\n|\r|\n)/, "").gsub(/(\')/, "&#x27;")
+  end
+end
